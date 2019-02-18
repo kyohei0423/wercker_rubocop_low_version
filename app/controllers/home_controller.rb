29fb19f2deb348086e4376f21456630213a94c66
kyohei0423 <kyo199333@gmail.com>
2019-02-18 12:50:10 +0900

    修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,3 @@
 class HomeController < ApplicationController
-  def index
-    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-  end
+  def index; end
 end
