fc54f10174771dddfbcaa365daaa8bf0d1a0b4c4
kyohei0423 <kyo199333@gmail.com>
2019-02-19 11:17:54 +0900

    だめコードを削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,2 @@
 module ApplicationHelper
-  def hoge_helper
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-    puts 'hoge'
-  end
 end
